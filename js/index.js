b1065ace5260be4c4c0de304e636dfb574adc8d5
Tyler Ang-Wanek <tylerw@axosoft.com>
2020-01-17 12:32:12 -0700

    Clean up unnecessary path normalization
    We have a different internal implementation in windows now that always expects windows paths

--- js/index.js
+++ js/index.js
@@ -1,28 +1,10 @@
 const { findGitRepos } = require('../build/Release/findGitRepos.node');
 
-const normalizeStartingPath = _path => {
-  const pathWithNormalizedSlashes = process.platform === 'win32'
-    ? _path.replace(/\\/g, '/')
-    : _path;
-
-  return pathWithNormalizedSlashes.replace(/\/+$/, '');
-};
-
-const normalizeRepositoryPath = _path => _path.replace(/\//g, '\\');
-
-const normalizePathCallback = callback => process.platform === 'win32'
-  ? paths => callback(paths.map(normalizeRepositoryPath))
-  : callback;
-
-module.exports = (startingPath, progressCallback, options = {}) => new Promise((resolve, reject) => {
+module.exports = (startingPath, progressCallback, { throttleTimeoutMS = 0 } = {}) => new Promise((resolve, reject) => {
   if (!startingPath && startingPath !== '') {
     reject(new Error('Must provide starting path as first argument.'));
     return;
   }
-
-  const {
-    throttleTimeoutMS = 0
-  } = options;
 
   if (!progressCallback) {
     reject(new Error('Must provide progress callback as second argument.'));
@@ -31,10 +13,10 @@ module.exports = (startingPath, progressCallback, options = {}) => new Promise((
 
   try {
     findGitRepos(
-      normalizeStartingPath(startingPath),
+      startingPath,
       throttleTimeoutMS,
-      normalizePathCallback(progressCallback),
-      normalizePathCallback(resolve)
+      progressCallback,
+      resolve
     );
   } catch (error) {
     reject(error);
